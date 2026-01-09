83f2ecd7d661825fb363d5dd2889d3bbac689e6e
yash dewasthale <yashdewasthale@ydev.local>
2026-01-09 17:20:52 +0530

    add theme

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DashboardLayout = (
+    {children}: {children: React.ReactNode}
+) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default DashboardLayout
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,10 @@ import { Button } from "@/components/ui/button";
 import Logout from "@/modules/components/logout";
 import { requireAuth } from "@/modules/components/utils/auth-utils";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
 
   await requireAuth()
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Logout>
-        <Button>Logout</Button>
-      </Logout>
-      
-    </div>
-  );
+  return redirect("/dashboard")
 }
