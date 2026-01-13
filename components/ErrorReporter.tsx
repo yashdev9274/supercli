a3cd8756aaec41efa4fbfcd29865be50fd69f3e4
yash dewasthale <yashdewasthale@ydev.local>
2026-01-13 19:29:59 +0530

    update dashboard layout

---
+++ components/ErrorReporter.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import React from 'react'
+
+export default function ErrorReporter() {
+  // ErrorReporter component for handling error reporting
+  // This can be extended with error boundary logic or error logging
+  return null
+}
